c6dadf00d25591beffa2289203ed96c4478e3797
thoner1 <thoner1@protonmail.com>
2024-03-04 14:14:34 -0500

    JFK Quote, randID, first if statement

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-cursedQuotes = (noun_1, noun_2, adjective) => {
-    
+cursedQuotes = (noun_1, noun_2, adjective_1, adjective_2) => {
+    quotes = ['"Ask not what your country can do for you, ask what you can do for your country" - John F. Kennedy']
+    randID = math.floor(math.random())
+    if (noun_1 || noun_2 || adjective_1 || adjective_2) {
+        console.log(noun_1);
+    } else {
+        console.log('You need to enter a noun!')
+    }
 }
+
+cursedQuotes('John');
