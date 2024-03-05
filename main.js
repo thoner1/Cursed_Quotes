180957122469569ebb3e56f3d339dcad9fd29898
thoner1 <thoner1@protonmail.com>
2024-03-05 16:22:55 -0500

    finished organizing quotes array

--- main.js
+++ main.js
@@ -4,24 +4,52 @@ cursedQuotes = (noun_1, noun_2, verb_1, verb_2) => {
 let randID = 1; //Math.floor(15*(Math.random()));
 
 /*------------------------------------------------------------------------ Enter Quotes Here -------------------------------------------------------------------------*/
-    const quotes = [['"Ask', 'not', 'what', 'your', 'country', 'can', 'do', 'for', 'you,', 'ask', 'what', 'you', 'can', 'do', 'for', 'your', 'country"', '-', 'John F. Kennedy'], 
+    const quotes = [
 //0
-    ['"For', 'everyone', 'that', 'exalts', 'himself', 'shall', 'be', 'humbled,', 'and', 'everyone', 'that', 'humbles', 'himself', 'shall', 'be', 'exalted"', '-', 'Jesus Christ'],
+    ['"Ask', 'not', 'what', 'your', 'country', 'can', 'do', 'for', 'you,', 'ask', 'what', 'you', 'can', 'do', 'for', 'your', 'country"', '-', 'John F. Kennedy'], 
+
 //1
-    ['"I', 'prefer', 'dangerous', 'freedom', 'over', 'peaceful', 'slavery"', '-', 'Thomas Jefferson'], ['"If', 'it', 'is', 'from', 'the', 'internet,', 'it', 'must', 'be', 'true."', '-', 'Abraham Lincoln'],
+    ['"For', 'everyone', 'that', 'exalts', 'himself', 'shall', 'be', 'humbled,', 'and', 'everyone', 'that', 'humbles', 'himself', 'shall', 'be', 'exalted"', '-', 'Jesus Christ'],
+
+//2
+    ['"I', 'prefer', 'dangerous', 'freedom', 'over', 'peaceful', 'slavery"', '-', 'Thomas Jefferson'], 
+
 //3
-    ['"Thats one small step for man, one giant leap for mankind" - Neil Armstrong'], '"The highest result of education is tolerance" - Helen Keller',
+    ['"If', 'it', 'is', 'from', 'the', 'internet,', 'it', 'must', 'be', 'true."', '-', 'Abraham Lincoln'],
+
+//4
+    ['"Thats', 'one', 'small', 'step', 'for', 'man,', 'one', 'giant', 'leap', 'for', 'mankind"', '-', 'Neil Armstrong'], 
+
+//5    
+    ['"The', 'highest', 'result', 'of', 'education', 'is', 'tolerance"', '-', 'Helen Keller'],
+
 //6
-    '"If you want to make your dreams come true, the first thing you have to do is wake up" - J.M. Power', 
+    ['"If', 'you', 'want', 'to', 'make', 'your', 'dreams', 'come', 'true,', 'the', 'first', 'thing', 'you', 'have', 'to', 'do', 'is', 'wake', 'up"', '-', 'J.M. Power'], 
+
 //7
-    '"Kindness is the language which the deaf can hear and the blind can see" - Mark Twain', '"The greatest pleasure in life is doing what people say you cannot do" - Walter Bagehot',
+    ['"Kindness', 'is', 'the', 'language', 'which', 'the', 'deaf', 'can', 'hear', 'and', 'the', 'blind', 'can', 'see"', '-', 'Mark Twain'], 
+
+//8    
+    ['"The', 'greatest', 'pleasure', 'in', 'life', 'is', 'doing', 'what', 'people', 'say', 'you', 'cannot', 'do"', '-', 'Walter Bagehot'],
+
 //9
-    '"Wise men speak because they have something to say; fools speak because they have to say something" - Plato', '"The important question is, how many hands have I shaked?" - George W. Bush', 
+    ['"Wise', 'men', 'speak', 'because', 'they', 'have', 'something', 'to', 'say;', 'fools', 'speak', 'because', 'they', 'have', 'to', 'say', 'something"', '-', 'Plato'], 
+
+//10    
+    ['"The', 'important', 'question', 'is,', 'how', 'many', 'hands', 'have', 'I', 'shaked?"', '-', 'George W. Bush'], 
+
 //11
-    '"I know how hard it is to put food on your family" - George W. Bush', '"I have never really wanted to go to Japan. Simply because I dont like eating fish. And I know that is very popular in Africa." - Britney Spears',
+    ['"I', 'know', 'how', 'hard', 'it', 'is', 'to', 'put', 'food', 'on', 'your', 'family"', '-', 'George W. Bush'], 
+
+//12
+    ['"I', 'have', 'never', 'really', 'wanted', 'to', 'go', 'to', 'Japan.', 'Simply', 'because', 'I', 'dont', 'like', 'eating', 'fish.', 'And', 'I', 'know', 'that', 'is', 'very', 'popular', 'in', 'Africa."', '-', 'Britney Spears'],
+
 //13
-    '"Smoking kills. If you are killed, you have lost an important part of your life." - Brooke Shields', '"No. I think gay marriage is something that should be between a man and a woman" - Arnold Schwarzenegger']
-//15
+    ['"Smoking', 'kills.', 'If', 'you', 'are', 'killed,', 'you', 'have', 'lost', 'an', 'important', 'part', 'of', 'your', 'life."', '-', 'Brooke Shields'], 
+
+//14    
+    ['"No.', 'I', 'think', 'gay', 'marriage', 'is', 'something', 'that', 'should', 'be', 'between', 'a', 'man', 'and', 'a', 'woman"', '-', 'Arnold Schwarzenegger']]
+
 
 let array = quotes[randID];
 
@@ -41,12 +69,12 @@ let array = quotes[randID];
 
 //randID === 2
         }else if(noun_1 && randID === 2 && !noun_2 && !verb_1 && !verb_2) {
-            array.splice(4,1,noun_1);
+            array.splice(3,1,noun_1);
             console.log(array.join(' '));
 
 //randID === 3
         }else if(noun_1 && randID === 3 && !noun_2 && !verb_1 && !verb_2) {
-            array.splice(4,1,noun_1);
+            array.splice(3,1,noun_1);
             console.log(array.join(' '));
 
 //randID === 4
