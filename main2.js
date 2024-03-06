2b86691f976f172cabfd335eb6ab6db0903e2171
thoner1 <thoner1@protonmail.com>
2024-03-06 11:30:04 -0500

    working on switch method

---
+++ main2.js
@@ -0,0 +1,17 @@
+if(noun_1 && randID === 0 && !noun_2 && !verb_1 && !verb_2) {
+    switch(randID) {
+        case 0:
+            array.splice(4,1,noun_1);
+            console.log(array.join(' '));
+            break;
+        case 1:
+            array.splice(4,1,noun_1);
+            console.log(array.join(' '));
+            break;
+        case 2:
+            array.splice(3,1,noun_1);
+            console.log(array.join(' '));
+            break;
+              
+    }
+}
