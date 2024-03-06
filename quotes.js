f05e34dc226afea9a26e852d65d7d1f2b33016c6
thoner1 <thoner1@protonmail.com>
2024-03-05 19:11:24 -0500

    copied quotes array

---
+++ quotes.js
@@ -0,0 +1,45 @@
+const quotes = [
+    //0
+        ['"Ask', 'not', 'what', 'your', 'country', 'can', 'do', 'for', 'you,', 'ask', 'what', 'you', 'can', 'do', 'for', 'your', 'country"', '-', 'John F. Kennedy'], 
+    
+    //1
+        ['"For', 'everyone', 'that', 'exalts', 'himself', 'shall', 'be', 'humbled,', 'and', 'everyone', 'that', 'humbles', 'himself', 'shall', 'be', 'exalted"', '-', 'Jesus Christ'],
+    
+    //2
+        ['"I', 'prefer', 'dangerous', 'freedom', 'over', 'peaceful', 'slavery"', '-', 'Thomas Jefferson'], 
+    
+    //3
+        ['"If', 'it', 'is', 'from', 'the', 'internet,', 'it', 'must', 'be', 'true."', '-', 'Abraham Lincoln'],
+    
+    //4
+        ['"Thats', 'one', 'small', 'step', 'for', 'man,', 'one', 'giant', 'leap', 'for', 'mankind"', '-', 'Neil Armstrong'], 
+    
+    //5    
+        ['"The', 'highest', 'result', 'of', 'education', 'is', 'tolerance"', '-', 'Helen Keller'],
+    
+    //6
+        ['"If', 'you', 'want', 'to', 'make', 'your', 'dreams', 'come', 'true,', 'the', 'first', 'thing', 'you', 'have', 'to', 'do', 'is', 'wake', 'up"', '-', 'J.M. Power'], 
+    
+    //7
+        ['"Kindness', 'is', 'the', 'language', 'which', 'the', 'deaf', 'can', 'hear', 'and', 'the', 'blind', 'can', 'see"', '-', 'Mark Twain'], 
+    
+    //8    
+        ['"The', 'greatest', 'pleasure', 'in', 'life', 'is', 'doing', 'what', 'people', 'say', 'you', 'cannot', 'do"', '-', 'Walter Bagehot'],
+    
+    //9
+        ['"Wise', 'men', 'speak', 'because', 'they', 'have', 'something', 'to', 'say;', 'fools', 'speak', 'because', 'they', 'have', 'to', 'say', 'something"', '-', 'Plato'], 
+    
+    //10    
+        ['"The', 'important', 'question', 'is,', 'how', 'many', 'hands', 'have', 'I', 'shaked?"', '-', 'George W. Bush'], 
+    
+    //11
+        ['"I', 'know', 'how', 'hard', 'it', 'is', 'to', 'put', 'food', 'on', 'your', 'family"', '-', 'George W. Bush'], 
+    
+    //12
+        ['"I', 'have', 'never', 'really', 'wanted', 'to', 'go', 'to', 'Japan','.', 'Simply', 'because', 'I', 'dont', 'like', 'eating', 'fish.', 'And', 'I', 'know', 'that', 'is', 'very', 'popular', 'in', 'Africa."', '-', 'Britney Spears'],
+    
+    //13
+        ['"Smoking', 'kills.', 'If', 'you', 'are', 'killed,', 'you', 'have', 'lost', 'an', 'important', 'part', 'of', 'your', 'life."', '-', 'Brooke Shields'], 
+    
+    //14    
+        ['"No.', 'I', 'think', 'gay', 'marriage', 'is', 'something', 'that', 'should', 'be', 'between', 'a', 'man', 'and', 'a', 'woman"', '-', 'Arnold Schwarzenegger']]
