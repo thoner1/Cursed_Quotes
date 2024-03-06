c7919e9cdf7706780c922939824d5f278f23d574
thoner1 <thoner1@protonmail.com>
2024-03-06 18:17:01 -0500

    noun_2, verb_1, and verb_2 finished

--- main.js
+++ main.js
@@ -929,7 +929,105 @@ let array = quotes[randID];
                 default:
                     console.log('Houston we have a problem');
                     break;
-            };            
+            };  
+            
+//------------ noun_2, verb_1, and verb_2 are entered only
+        }else if(noun_2 && verb_1 && verb_2 && !noun_1) {
+            switch(randID) {
+                case 0:
+                    array.splice(16,1,noun_2);
+                    array.splice(6,1, verb_1);
+                    array.splice(9,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 1:
+                    array.splice(12,1,noun_2);
+                    array.splice(3,1, verb_1);
+                    array.splice(11,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 2:
+                    array.splice(6,1,noun_2);
+                    array.splice(1,1, verb_1);
+                    array.splice(4,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 3:
+                    array.splice(5,1,noun_2);
+                    array.splice(3,1, verb_1);
+                    array.splice(7,1, verb_2);
+                    console.log(array.join(' ')); 
+                    break;
+                case 4:
+                    array.splice(10,1,noun_2);
+                    array.splice(4,1, verb_1);
+                    array.splice(8,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 5:
+                    array.splice(4,1,noun_2);
+                    array.splice(1,1, verb_1);
+                    array.splice(6,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 6:
+                    array.splice(12,1,noun_2);
+                    array.splice(4,1, verb_1);
+                    array.splice(17,2, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 7:
+                    array.splice(3,1,noun_2);
+                    array.splice(8,1, verb_1);
+                    array.splice(13,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 8:
+                    array.splice(8,1,noun_2);
+                    array.splice(6,1, verb_1);
+                    array.splice(12,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 9:
+                    array.splice(9,1,noun_2);
+                    array.splice(2,1, verb_1);
+                    array.splice(10,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 10:
+                    array.splice(6,1,noun_2);
+                    array.splice(3,1, verb_1);
+                    array.splice(9,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 11:
+                    array.splice(11,1,noun_2);
+                    array.splice(7,1, verb_1);
+                    array.splice(8,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 12:
+                    array.splice(16,1,noun_2);
+                    array.splice(6,1, verb_1);
+                    array.splice(15,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 13:
+                    array.splice(14,1,noun_2);
+                    array.splice(1,1, verb_1);
+                    array.splice(8,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                case 14:
+                    array.splice(15,1,noun_2);
+                    array.splice(2,1, verb_1);
+                    array.splice(9,1, verb_2);
+                    console.log(array.join(' '));
+                    break;
+                default:
+                    console.log('Houston we have a problem');
+                    break;
+            };
         }
 /*---------------------------------------------------------------------- Random Modified Quote -----------------------------------------------------------------------*/
     }else if (noun_2) {
